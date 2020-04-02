a6e0f06a617b2fa9974d5ccd0adefa8e6ed9a5bc
RalphLincoln <ialaneme@yahoo.com>
2020-04-02 03:03:01 +0100

    Committing my exercise on react props

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Person = (Props) => {
+  console.log(Props);
+  return (
+    <div className="person">
+      <img src={`https://randomuser.me/api/portraits/med/men/${Props.img}.jpg`} alt="Men-Pictures" />
+      <p>{Props.name}</p>
+      <p>{Props.job}</p>
+      {Props.children}
+    </div>
+  )
+}
+
+
+
+const PersonsList = () => {
+  return (
+    <div className="personList">
+      <Person img="35" name="Paul" job="Developer">
+        <p>A passionate and enticing fellow.........</p>
+      </Person>
+      <Person img="10" name="John" job="Designer" />
+      <Person img="45" name="Mark" job="Boss" />
+    </div>
+  )
+}
+
+
+
+ReactDOM.render(<PersonsList />, document.getElementById("root"));
